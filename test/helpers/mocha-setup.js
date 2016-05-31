ef0cc3d74232f947c51237f27d1c56a6ebb4f5b1
Matt Travi <programmer@travi.org>
2016-05-30 23:36:53 -0500

    configured helpers for mocha

---
+++ test/helpers/mocha-setup.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import referee from 'referee';
+
+sinon.assert.expose(chai.assert, { prefix: '' });
+
+referee.format = require('formatio').configure({quoteStrings: false}).ascii;
+require('referee-sinon')(referee, sinon);
+global.formatio = require('formatio');
